0e667acd9b1f9de4637d2f90eaa3563bcf389371
Bonkesam <bonkesam9@gmail.com>
2023-05-25 17:38:12 +0300

    Adds the social logins to the Auth Form

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,6 +4,9 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import Input from '@/app/components/inputs/Input';
+import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -49,6 +52,7 @@ const AuthForm = () => {
         setIsLoading(true);
     }
 
+    
   return (
     <div className="
       mt-8
@@ -68,8 +72,73 @@ const AuthForm = () => {
             className="space-y-6"
             onSubmit={handleSubmit(onSubmit)}
           >
-            <Input label="Email" />
+            {variant === 'REGISTER' && (
+                <Input id='name' label="Name" register={register} errors={errors} />
+            )}
+                <Input id='email' label="Email Address" type='email' register={register} errors={errors} />
+
+                <Input id='password' label="Password" type='password' register={register} errors={errors} />
+                <div>
+                    <Button 
+                        disabled={isLoading} 
+                        fullWidth 
+                        type="submit"
+                    >
+                        {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+                    </Button>
+                </div>
           </form>
+
+          <div className="mt-6">
+            <div className="relative">
+                <div className="absolute
+                inset-0
+                flex
+                items-center">
+                    <div className="w-full border-t order-gray-300" />                   
+                </div>
+
+                <div className="relative flex justify-center text-sm">
+                    <span className="bg-white px-2 text-gray-500">
+                        or Continue with
+                    </span>
+                </div>
+            </div>
+
+            <div className="mt-6 flex gap-2">
+                <AuthSocialButton 
+                  icon={BsGithub}
+                  onClick={() => socialAction('github')}
+                />
+
+                <AuthSocialButton 
+                  icon={BsGoogle}
+                  onClick={() => socialAction('google')}
+                />
+
+            </div>
+          </div>
+
+          <div className="
+            flex
+            gap-2
+            justify-center
+            text-sm
+            mt-6
+            px-2
+            text-gray-500
+          ">
+            <div>
+                {variant ==='LOGIN' ? 'New to Messenger?' : 'Already have an Account?'}
+            </div>
+            <div 
+              onClick={toggleVariant}
+              className="underline cursor-pointer"
+            >
+                {variant === 'LOGIN' ? 'Create an Account' : 'Login'}
+            </div>
+
+          </div>
        </div>
     </div>
   )
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -38,6 +38,38 @@ const input: React.FC<InputProps> = ({
       ' htmlFor={id}>
         {label}
       </label>
+
+      <div className='mt-2'>
+        <input 
+          id={id}
+          type={type}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id, {required})}
+          className={clsx(`
+            form-input
+            block
+            w-full
+            rounded-md
+            border-0
+            py-1.5text-gray-00
+            shadow-sm
+            ring-1
+            ring-inset
+            ring-gray-300
+            placeholder:text-gray-400
+            focus:ring-2
+            focus:ring-inset
+            focus:ring-sky-600
+            sm:text-sm
+            sm:leading-6,
+            errors[id] && "focus:ring-rose-600"`,
+            disabled && "opacity-50 cursor-default"
+
+          )}
+        />
+
+      </div>
     </div>
   )
 }
